d33f9c72dd6e488d9155e32a9f57612947d5b109
Paddy Mullen <paddy@chartwidget.com>
2010-07-24 14:12:41 -0400

    working on plumbing in clojure

--- paddy-slime-config.el
+++ paddy-slime-config.el
@@ -13,4 +13,29 @@
 (slime-setup '(slime-fancy))
 (add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
 (add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
+
+
+;; clojure-mode
+(add-to-list 'load-path "~/code/lisp/clojure-mode")
+(require 'clojure-mode)
+
+;; swank-clojure
+;(add-to-list 'load-path "~/opt/swank-clojure/src/emacs")
+
+;(setq swank-clojure-jar-path "~/.clojure/clojure.jar"
+;      swank-clojure-extra-classpaths (list
+;				      "~/opt/swank-clojure/src/main/clojure"
+;				      "~/.clojure/clojure-contrib.jar"))
+
+;(require 'swank-clojure-autoload)
+
+;; slime
+;(eval-after-load "slime" 
+;  '(progn (slime-setup '(slime-repl))))
+
+;(add-to-list 'load-path "~/opt/slime")
+;(require 'slime)
+;(slime-setup) 
+
+
 (provide 'paddy-slime-config)
