7e72e76f3d3314fe71273933d4d5488c55c5c171
Paddy Mullen <paddy@chartwidget.com>
2010-07-24 12:53:05 -0400

    formatting, added auto-fill-mode to all org buffers

--- paddy-org.el
+++ paddy-org.el
@@ -1,4 +1,11 @@
-     (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
-     (global-set-key "\C-cl" 'org-store-link)
-     (global-set-key "\C-ca" 'org-agenda)
-     (global-set-key "\C-cb" 'org-iswitchb)
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+(add-hook 'org-mode-hook
+	  '(lambda ()
+             (auto-fill-mode)))
+
+(provide 'paddy-org)
+
